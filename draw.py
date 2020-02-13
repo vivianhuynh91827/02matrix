f81922accf00e7ba903f05f460c7724d8b77ef30
vivianhuynh91827 <vhuynh00@stuy.edu>
2020-02-13 12:44:09 -0500

    assignment completed

--- draw.py
+++ draw.py
@@ -2,17 +2,18 @@ from display import *
 from matrix import *
 
 
-def draw_lines( matrix, screen, color ):
-    pass
+def draw_lines( matrix, s, c):
+    for i in range(int(len(matrix) /2)):
+        p1 = matrix[2*i]
+        p2 = matrix[2*i + 1]
+        draw_line(p1[0], p1[1], p2[0], p2[1], s, c)
 
 def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
-    pass
+    matrix.append([x0,y0,z0,1])
+    matrix.append([x1,y1,z1,1])
 
 def add_point( matrix, x, y, z=0 ):
-    pass
-
-
-
+    matrix.append([x,y,z,1])
 
 def draw_line( x0, y0, x1, y1, screen, color ):
 
@@ -34,7 +35,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     if ( abs(x1-x0) >= abs(y1 - y0) ):
 
         #octant 1
-        if A > 0:            
+        if A > 0:
             d = A + B/2
 
             while x < x1:
--- main.py
+++ main.py
@@ -2,10 +2,31 @@ from display import *
 from draw import *
 from matrix import *
 
-screen = new_screen()
-color = [ 0, 255, 0 ]
+s = new_screen()
+c = [ 0, 255, 0 ]
 matrix = new_matrix()
 
+add_edge(matrix, 50, 450, 0, 100, 450, 0);
+add_edge(matrix, 50, 450, 0, 50, 400, 0);
+add_edge(matrix, 100, 450, 0, 100, 400, 0);
+add_edge(matrix, 100, 400, 0, 50, 400, 0);
 
-draw_lines( matrix, screen, color )
-display(screen)
+add_edge(matrix, 200, 450, 0, 250, 450, 0);
+add_edge(matrix, 200, 450, 0, 200, 400, 0);
+add_edge(matrix, 250, 450, 0, 250, 400, 0);
+add_edge(matrix, 250, 400, 0, 200, 400, 0);
+
+add_edge(matrix, 150, 400, 0, 130, 360, 0);
+add_edge(matrix, 150, 400, 0, 170, 360, 0);
+add_edge(matrix, 130, 360, 0, 170, 360, 0);
+
+add_edge(matrix, 100, 340, 0, 200, 340, 0);
+add_edge(matrix, 100, 320, 0, 200, 320, 0);
+add_edge(matrix, 100, 340, 0, 100, 320, 0);
+add_edge(matrix, 200, 340, 0, 200, 320, 0);
+
+draw_lines( matrix, s, c )
+
+save_ppm(s, 'binary.ppm')
+save_ppm_ascii(s, 'ascii.ppm')
+save_extension(s, 'img.png')
